634abf33ad3b50f83751bc3a8ac7de1e30e8f871
bedinin <bedinin@gmail.com>
2021-11-10 17:14:40 +1000

    43

--- learn.js
+++ learn.js
@@ -1,5 +1,4 @@
-const myList = [["purchase 1", 1],
-    ["purchase 2", 2],
-    ["purchase 3", 3],
-    ["purchase 4", 4],
-    ["purchase 5", 5]];
+function reusableFunction () {
+    console.log("Hi World");
+}
+reusableFunction ();
