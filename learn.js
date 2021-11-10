ed2974d25e5d02706754a03ebf0a89d5bf7774db
bedinin <bedinin@gmail.com>
2021-11-10 16:10:52 +1000

    33

--- learn.js
+++ learn.js
@@ -1,4 +1,5 @@
 // Setup
 const lastName = "Lovelace";
+
 // Only change code below this line
-const lastLetterOfLastName = lastName[lastName.length - 1];
+const secondToLastLetterOfLastName = lastName[lastName.length -2]; // Change this line
