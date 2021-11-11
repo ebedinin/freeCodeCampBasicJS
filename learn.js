0d3f06fdc09927c4e986c80c49dceff2e6c6fee0
bedinin <bedinin@gmail.com>
2021-11-11 14:58:41 +1000

    69

--- learn.js
+++ learn.js
@@ -1,24 +1,26 @@
-function switchOfStuff(val) {
+function sequentialSizes(val) {
     let answer = "";
-    switch (val) {
-        case "a":{
-            answer = "apple";
+    switch(val){
+        case 1:
+        case 2:
+        case 3:{
+            answer = "Low";
             break;
         }
-        case "b":{
-            answer = "bird";
+        case 4:
+        case 5:
+        case 6:{
+            answer = "Mid";
             break;
         }
-        case "c":{
-            answer = "cat";
-            break;
-        }
-        default:{
-            answer = "stuff";
+        case 7:
+        case 8:
+        case 9:{
+            answer = "High";
             break;
         }
     }
     return answer;
 }
 
-switchOfStuff(1);
+sequentialSizes(1);
