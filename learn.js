4bedd9ed6525ae6d0d1ac82852f97431c01418ca
bedinin <bedinin@gmail.com>
2021-11-11 14:50:06 +1000

    68

--- learn.js
+++ learn.js
@@ -1,24 +1,24 @@
-function caseInSwitch(val) {
+function switchOfStuff(val) {
     let answer = "";
     switch (val) {
-        case 1:{
-            answer = "alpha";
+        case "a":{
+            answer = "apple";
             break;
         }
-        case 2:{
-            answer = "beta";
+        case "b":{
+            answer = "bird";
             break;
         }
-        case 3:{
-            answer = "gamma";
+        case "c":{
+            answer = "cat";
             break;
         }
-        case 4:{
-            answer = "delta";
+        default:{
+            answer = "stuff";
             break;
         }
     }
     return answer;
 }
 
-caseInSwitch(1);
+switchOfStuff(1);
