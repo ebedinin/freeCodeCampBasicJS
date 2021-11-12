a6095bf07548dfe00911c0a1fdcc3fcfbac22b68
ebedinin <bedinin@gmail.com>
2021-11-12 22:22:10 +1000

    72

--- learn.js
+++ learn.js
@@ -1,7 +1,7 @@
-function isLess(a, b) {
-    // Only change code below this line
-    return a < b;
-    // Only change code above this line
+// Setup
+function abTest(a, b) {
+    if (a < 0 || b < 0) return undefined;
+    return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
 }
 
-isLess(10, 15);
+abTest(2, 2);
