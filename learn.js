c88a8f8053e56d255ea2604c4656093764af0a04
ebedinin <bedinin@gmail.com>
2021-11-13 15:23:42 +1000

    73

--- learn.js
+++ learn.js
@@ -1,45 +1,6 @@
-let count = 0;
-
-function cc(card) {
-    // Only change code below this line
-    switch (card) {
-        case 2:
-        case 3:
-        case 4:
-        case 5:
-        case 6: {
-            count++;
-            break;
-        }
-        case 7:
-        case 8:
-        case 9: {
-            break;
-        }
-        case 10:
-        case 'J':
-        case 'Q':
-        case 'K':
-        case 'A': {
-            count--;
-            break;
-        }
-    }
-    if (count > 0)
-        return count.toString() + " Bet";
-    else if (count < 0)
-        return count.toString() + " Hold";
-    else
-        return count.toString() + " Hold";
-}
-
-let c = cc(2);
-console.log(c);
-c = cc('J');
-console.log(c);
-c = cc(9);
-console.log(c);
-c = cc(2);
-console.log(c);
-c = cc(7);
-console.log(c);
+const myDog = {
+    "name": "Bob",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["Sharik", "tuzik"]
+};
