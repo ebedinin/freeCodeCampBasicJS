d4431f0e4089679208aa75935c4da7a2a9889f1b
ebedinin <bedinin@gmail.com>
2021-11-13 16:40:48 +1000

    82

--- learn.js
+++ learn.js
@@ -1,21 +1,13 @@
-const myMusic = [
-    {
-        "artist": "Billy Joel",
-        "title": "Piano Man",
-        "release_year": 1973,
-        "formats": [
-            "CD",
-            "8T",
-            "LP"
-        ],
-        "gold": true
+const myStorage = {
+    "car": {
+        "inside": {
+            "glove box": "maps",
+            "passenger seat": "crumbs"
+        },
+        "outside": {
+            "trunk": "jack"
+        }
     }
-];
-myMusic.push({
-    "artist": "A",
-    "title": "B",
-    "release_year": 1988,
-    "formats": ["8T",
-        "LP"
-    ]
-});
+};
+
+const gloveBoxContents = myStorage.car.inside["glove box"];
