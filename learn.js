04547b6957055870256d710c5fe3fcbc29f3480a
ebedinin <bedinin@gmail.com>
2021-11-13 15:35:36 +1000

    77

--- learn.js
+++ learn.js
@@ -1,10 +1,9 @@
-playerNumber// Setup
-const testObj = {
-    12: "Namath",
-    16: "Montana",
-    19: "Unitas"
+// Setup
+const myDog = {
+    "name": "Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"]
 };
-
+myDog.name = "Happy Coder";
 // Only change code below this line
-const playerNumber = 16;  // Change this line
-const player = testObj[playerNumber];   // Change this line
