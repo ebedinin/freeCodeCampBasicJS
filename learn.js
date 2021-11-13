c23f24f02e59f8d5e9accbd945569f45eb1fe5dd
ebedinin <bedinin@gmail.com>
2021-11-13 15:00:33 +1000

    73

--- learn.js
+++ learn.js
@@ -1,7 +1,45 @@
-// Setup
-function abTest(a, b) {
-    if (a < 0 || b < 0) return undefined;
-    return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+let count = 0;
+
+function cc(card) {
+    // Only change code below this line
+    switch (card) {
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6: {
+            count++;
+            break;
+        }
+        case 7:
+        case 8:
+        case 9: {
+            break;
+        }
+        case 10:
+        case 'J':
+        case 'Q':
+        case 'K':
+        case 'A': {
+            count--;
+            break;
+        }
+    }
+    if (count > 0)
+        return count.toString() + " Bet";
+    else if (count < 0)
+        return count.toString() + " Hold";
+    else
+        return count.toString() + " Hold";
 }
 
-abTest(2, 2);
+let c = cc(2);
+console.log(c);
+c = cc('J');
+console.log(c);
+c = cc(9);
+console.log(c);
+c = cc(2);
+console.log(c);
+c = cc(7);
+console.log(c);
